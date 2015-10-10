001b276d1c8e57832d6f4a2ab4970bde1705f28a
Alex Cevallos <Alex.Cevallos@alaskaair.com>
2015-10-10 12:46:37 -0700

    Added xcuserdata and playground for testing

---
+++ Stormy/MyPlayground1.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var testa : String?
+print(testa)
+if let testo = testa {
+    print("hi")
+}
