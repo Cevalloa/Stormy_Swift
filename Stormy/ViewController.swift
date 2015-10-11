7d07beb258ef3c27cedfc050714430a5e2be353a
Alex Cevallos <alex.cevallos@alaskaair.com>
2015-10-11 11:42:37 -0700

    - Established network connection using multi threading - Minor cleanups throughout project settings

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -14,21 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentHumidityLabel: UILabel?
     @IBOutlet weak var currentPrecipitationLabel: UILabel?
     
+    private let forecastAPIKey = "bdf30eab98fdf609e887c18b37a7b694"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Extract data from the pList into the dictionary
-        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
-            let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
-            let currentWeatherDictionary = weatherDictionary["currently"] as? [String : AnyObject]{
+        let baseURL = NSURL(string: "https://api.forecast.io/\(forecastAPIKey)/")
+        
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+
+        // Use NSURLSession API to fetch data
+        let defaultConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: defaultConfiguration)
+        
+        // NSURLRequest object
+        let request = NSURLRequest(URL: forecastURL!)
+        
+        let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
             
-                let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
-                
-                currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
-                currentHumidityLabel?.text = "\(currentWeather.humidity)%"
-                currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
-        }
+            println(data)
+        })
+        
+        dataTask.resume()
     }
 }
 
